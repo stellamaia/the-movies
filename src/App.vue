fb7c2ad4750dd16e6b0f8180b8a0069664e1ea88
stella <stellamaia97@hotmail.com>
2023-05-18 23:42:00 -0300

    Implementação de css

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,16 +18,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
