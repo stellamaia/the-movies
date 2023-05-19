570c03e49d2a3b0ef4e0ab372b88b360b05cdad2
stella <stellamaia97@hotmail.com>
2023-05-18 23:40:29 -0300

    Atualização de estilização da navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="nav-bar-component">
-    <nav class="navbar container-nav">
-      <router-link to="/">
-      <i class="material-symbols-outlined menu">menu</i>
-    </router-link> 
+    <nav class="navbar  fixed-top container-nav">
+    
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+        aria-label="Toggle navigation"
+      >
+        <i class="material-symbols-outlined menu"> Menu </i>
+      </button>
+
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >favoritos</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
 
       <router-link class="icon" to="/login">
         <i class="material-symbols-outlined icon person">person</i>
       </router-link>
+ 
     </nav>
   </div>
 </template>
@@ -22,14 +60,14 @@ export default {
 </script>
 
 <style lang="scss">
-.nav-bar-component{
-  background-color: #000000;
+.nav-bar-component {
+  background-color: #030328;
 }
 .container-nav {
-  display: flex;
   padding: 15px;
+
 }
-.menu{
+.menu {
   color: white;
   font-size: 32px;
 }
@@ -42,8 +80,13 @@ export default {
   height: 40px;
   width: 40px;
 
-  &person{
-    font-size: 32px;
+  &.person {
+    font-size: 29px;
+    padding-top: 3px;
   }
 }
+.offcanvas.offcanvas-start {
+  width: 40%;
+}
+
 </style>
